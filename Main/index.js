d032beeef15cbeb1f040be2ec9bad2dc40233f60
atulit gaur <atulit23@gmail.com>
2024-03-23 16:33:34 +0530

    main

--- Main/index.js
+++ Main/index.js
@@ -20,7 +20,13 @@ const router = express.Router();
 const PORT = 8001;
 
 app.use(express.json());
-app.use(cors());
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,           
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 io.on("connection", (socket) => {
   console.log("client connected: ", socket.id);
